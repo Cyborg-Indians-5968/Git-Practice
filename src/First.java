3db6a968e0028ff86f4feb97ed26b926bdf5d3c5
Mattyc00 <mchoi1218@gmail.com>
2017-09-11 23:51:26 -0500

    Matt's PR

--- src/First.java
+++ src/First.java
@@ -4,6 +4,15 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("First commit");
+		
+		int lit = 6;
+		
+		while(lit < 10){
+			System.out.println("hello");
+			lit++;
+		}
+		
+		System.out.println("bye");
+		
 	}
-
-}
+}//does this work out
